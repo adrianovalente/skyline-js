b4f030850e771be3267034d847412a924c45b35a
drico <dri9595@gmail.com>
2018-01-13 11:51:11 -0200

    added auth module

---
+++ src/auth.js
@@ -0,0 +1,36 @@
+import request from 'request'
+import Promise from 'bluebird'
+
+const AUTH_URL = 'https://www.skyline.com.br/cloud/multi/skyline-web-gateway/login'
+
+/**
+ * Gets the auth token for a fiven nexxera instance
+ *
+ * @param {nexxera}
+ * @returns {Promise}
+ */
+export function getAuthToken (nexxera) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: AUTH_URL,
+      method: 'post',
+      form: {
+        username: nexxera.username,
+        password: nexxera.password
+      }
+    }, (err, res, body) => {
+      if (err) return reject(err)
+      if (res.statusCode !== 200) return reject(new Error(`statusCode ${res.statusCode}: ${body}`))
+      
+      try {
+        const token = res.headers['set-cookie'][0]
+          .split('session=')[1]
+          .split(';')[0]
+          
+        return resolve(`firstAccess=false; defaultServer=2; currentUser=${nexxera.username}; session=${token}`)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-export default {
-  hello: 'world'
-}
+import * as auth from './auth'
 
-console.log('Hello World!')
+export default class Nexxera {
+
+  constructor (credentials = {}) {
+    const { username, password } = credentials
+    if (!username || !password) throw new Error('Please provide username and password when initializing!')
+
+    this.username = username
+    this.password = password
+  }
+
+  _authenticate () {
+    const self = this
+    return auth.getAuthToken(self)
+      .then(token => {
+        self.token = token
+        return self
+      })
+  }
+
+}
